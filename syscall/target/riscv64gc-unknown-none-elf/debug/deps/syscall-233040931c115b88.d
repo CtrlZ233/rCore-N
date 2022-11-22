a3cfd4cb20e5c9a92bdb3748abf86b4e488f32c3
zflcs <1491657576@qq.com>
2022-11-22 10:15:57 +0800

    删除 target

--- syscall/target/riscv64gc-unknown-none-elf/debug/deps/syscall-233040931c115b88.d
+++
@@ -1,6 +0,0 @@
-/home/zfl/rv-n-ext-impl/rCore-N/syscall/target/riscv64gc-unknown-none-elf/debug/deps/syscall-233040931c115b88.rmeta: src/lib.rs src/syscall_asm.rs
-
-/home/zfl/rv-n-ext-impl/rCore-N/syscall/target/riscv64gc-unknown-none-elf/debug/deps/syscall-233040931c115b88.d: src/lib.rs src/syscall_asm.rs
-
-src/lib.rs:
-src/syscall_asm.rs:
