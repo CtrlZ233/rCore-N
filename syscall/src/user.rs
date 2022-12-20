0e31967f8820e2795046fc4d2586fb66a7ca40c3
CtrlZ233 <328825619@qq.com>
2022-12-21 02:21:08 +0800

    add sync test

--- syscall/src/user.rs
+++ syscall/src/user.rs
@@ -268,3 +268,31 @@ pub fn sys_condvar_wait(condvar_id: usize, mutex_id: usize) -> isize {
     }
 }
 
+pub fn mutex_create() -> isize {
+    sys_mutex_create(false)
+}
+
+pub fn mutex_blocking_create() -> isize {
+    sys_mutex_create(true)
+}
+
+pub fn mutex_lock(mutex_id: usize) {
+    sys_mutex_lock(mutex_id);
+}
+
+pub fn mutex_unlock(mutex_id: usize) {
+    sys_mutex_unlock(mutex_id);
+}
+
+pub fn condvar_create() -> isize {
+    sys_condvar_create(0)
+}
+
+pub fn condvar_signal(condvar_id: usize) {
+    sys_condvar_signal(condvar_id);
+}
+
+pub fn condvar_wait(condvar_id: usize, mutex_id: usize) {
+    sys_condvar_wait(condvar_id, mutex_id);
+}
+
