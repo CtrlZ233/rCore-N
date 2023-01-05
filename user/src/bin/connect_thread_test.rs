5f98de5a674f239cb8c60f3cb5200f4598924534
CtrlZ233 <328825619@qq.com>
2023-01-06 07:59:12 +0800

    debug

--- user/src/bin/connect_thread_test.rs
+++ user/src/bin/connect_thread_test.rs
@@ -44,27 +44,28 @@ pub fn main() -> i32 {
         "[hello world] trap init result: {:#x}, pid: {}, cost time: {} ms",
         init_res, pid, end - start
     );
-    let max_connection = 128;
+    let max_connection = 256;
     let mut waits = Vec::new();
     for _ in 0..max_connection {
-        waits.push(build_connect(0));
+        let (s_tid, c_tid) = build_connect(0);
+        waits.push(s_tid);
+        waits.push(c_tid);
     }
 
     for tid in waits.iter() {
         waittid(*tid);
     }
 
-    
-    
     // sleep(1000);
     0
 }
 
-pub fn build_connect(prio: usize) -> usize {
+pub fn build_connect(prio: usize) -> (usize, usize) {
     let mut fds = [0usize; 2];
     pipe(&mut fds);
-    add_coroutine(Box::pin(client(fds[1])), 0);
-    thread_create(server as usize, fds[0]) as usize
+    let s_tid = thread_create(server as usize, fds[0]) as usize;
+    let c_tid = thread_create(client as usize, fds[1]) as usize;
+    (s_tid, c_tid)
 }
 
 fn server(server_fd: usize) {
@@ -90,67 +91,36 @@ fn server(server_fd: usize) {
     
 }
 
-// fn client(client_fd: usize) {
+// async fn client(client_fd: usize) {
 //     // println!("client start");
 //     let req = DATA_C;
-//     let current = get_time();
+//     let cid = current_cid();
 //     unsafe {
 //         while true && (get_time() as usize) < (START_TIME + RUN_TIME_LIMIT) {
-//             while current + SEND_TIMER > get_time() {
-//                 yield_();
-//             }
-//             current = get_time();
-//             write(client_fd, req.as_bytes().as_ptr() as usize);
+//             let helper = TimerHelper::new();
+//             helper.await;
+            
+//             write(client_fd, &req.as_bytes());
+            
 //         }
 //     }
 //     close(client_fd);
 // }
 
-async fn client(client_fd: usize) {
+fn client(client_fd: usize) {
     // println!("client start");
     let req = DATA_C;
-    let cid = current_cid();
+    let mut current_time = get_time() as usize;
     unsafe {
         while true && (get_time() as usize) < (START_TIME + RUN_TIME_LIMIT) {
-            let helper = TimerHelper::new();
-            helper.await;
-            
+            while current_time + SEND_TIMER > get_time() as usize {
+                yield_();
+            }
+            current_time = get_time() as usize;
             write(client_fd, &req.as_bytes());
             
         }
     }
     close(client_fd);
-}
-
-
-struct TimerHelper {
-    time: usize,
-}
-
-impl TimerHelper {
-    fn new() -> Self {
-        TimerHelper {
-            time: get_time() as usize,
-        }
-    }
-}
-
-impl Future for TimerHelper {
-    type Output = ();
-
-    fn poll(mut self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
-        // submit async task to kernel and return immediately
-        let cur_time = get_time() as usize;
-        if self.time + SEND_TIMER > cur_time {
-            // println!("send start: {}", current_cid());
-            set_cid_timer(((self.time + SEND_TIMER) * 1000) as isize, current_cid());
-            return Poll::Pending;
-        }
-        return Poll::Ready(());
-    }
-}
-
-#[no_mangle]
-pub fn wake_handler(cid: usize) {
-    re_back(cid);
+    exit(2);
 }
